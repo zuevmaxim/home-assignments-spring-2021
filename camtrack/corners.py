21a38451695c24f10e4dfc1db6278a22d43c4b9e
maxim <max-zu99@mail.ru>
2021-02-06 22:30:11 +0300

    Complete hw1

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -46,17 +46,55 @@ class _CornerStorageBuilder:
         return StorageImpl(item[1] for item in sorted(self._corners.items()))
 
 
+from numba import jit
+
+
+@jit
+def id_closer_than(array, value, distance):
+    d = distance
+    for x in array:
+        if abs(x[0] - value[0]) < d and abs(x[1] - value[1]) < d:
+            return x[2]
+    return array.shape[0]
+
+
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    # TODO
+    maxCorners = 250
+    qualityLevel = 0.001
+    minDistance = 20
+    blockSize = 9
+    winSize = (20, 20)
+
     image_0 = frame_sequence[0]
-    corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
-    )
+    corner_points = cv2.goodFeaturesToTrack(image_0, maxCorners, qualityLevel, minDistance, blockSize=blockSize,
+                                            useHarrisDetector=False).reshape((-1, 2))
+    ids = np.arange(0, len(corner_points))
+    corners = FrameCorners(ids, corner_points, np.full(corner_points.shape, (blockSize, blockSize)))
     builder.set_corners_at_frame(0, corners)
+    max_id = ids.max()
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
+        nextPts, status, _ = cv2.calcOpticalFlowPyrLK(
+            np.asarray(image_0 * 256, np.uint8),
+            np.asarray(image_1 * 256, np.uint8),
+            corner_points.astype(np.float32), None, winSize=winSize)
+        ptid = np.c_[nextPts, ids][status.reshape(-1) == 1]
+        b = np.fromiter((id_closer_than(ptid, xi, minDistance) >= xi[2] for xi in ptid), bool, count=len(ptid))
+        ptid = ptid[b]
+        corner_points, ids = np.hsplit(ptid, [2])
+        ids = ids.reshape(-1).astype(int)
+
+        new_corner_points = cv2.goodFeaturesToTrack(image_1, maxCorners, qualityLevel, minDistance, blockSize=blockSize,
+                                                    useHarrisDetector=False).reshape((-1, 2))
+        b = np.fromiter((id_closer_than(ptid, xi, minDistance) == ptid.shape[0] for xi in new_corner_points), bool,
+                        count=len(new_corner_points))
+        new_corner_points = new_corner_points[b][:maxCorners - corner_points.shape[0]]
+        new_ids = np.arange(max_id, max_id + new_corner_points.shape[0])
+        max_id += new_corner_points.shape[0]
+        corner_points = np.concatenate((corner_points, new_corner_points))
+        ids = np.concatenate((ids, new_ids))
+
+        corners = FrameCorners(ids, corner_points, np.full(corner_points.shape, (blockSize, blockSize)))
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
 
